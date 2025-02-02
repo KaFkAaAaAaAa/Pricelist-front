7ea1372066544effda23129e3ba85c2badfcc52e
user <dupa@dupa>
2025-02-02 21:21:46 +0100

    fixing couple things and started adding translation

--- Pricelist/settings.py
+++ Pricelist/settings.py
@@ -122,8 +122,12 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
