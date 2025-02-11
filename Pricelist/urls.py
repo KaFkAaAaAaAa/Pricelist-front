5918b4bb920d8a21bf2dd114f1ecd6278950c571
user <dupa@dupa>
2025-02-11 23:08:38 +0100

    i hope it will work

--- Pricelist/urls.py
+++ Pricelist/urls.py
@@ -125,3 +125,6 @@ urlpatterns = (
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.BASE_DIR / "static")
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
