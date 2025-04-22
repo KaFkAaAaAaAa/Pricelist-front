3898111e23d52a4f165e57663ec02475924a520e
Kasper <gozdzik.kasper@gmail.com>
2025-04-22 18:49:21 +0200

    group fix

--- transactions/views.py
+++ transactions/views.py
@@ -13,13 +13,20 @@ from django.utils.translation import gettext_lazy as _
 
 from items.views import _add_items_to_offer, _make_price_list
 from pdfgenerator.views import generate_pdf
-from Pricelist.settings import ADMIN_GROUPS, API_BASE_URL, CLIENT_GROUPS, SKU_REGEX
+from Pricelist.settings import (
+    ADMIN_GROUPS,
+    API_BASE_URL,
+    CLIENT_GROUPS,
+    SKU_REGEX,
+    SUPPORT_GROUPS,
+)
 from Pricelist.utils import (
     Page,
     _amount_to_display,
     _amount_to_float,
     _amount_to_store,
     _api_error_interpreter,
+    _get_group,
     _get_headers,
     _get_page_param,
     _is_admin,
@@ -820,11 +827,13 @@ def create_final(request, data, headers):
 
 
 @require_auth
-@require_group(ADMIN_GROUPS + CLIENT_GROUPS)
+@require_group(ADMIN_GROUPS + CLIENT_GROUPS + SUPPORT_GROUPS)
 def change_status(request, transaction_uuid):
     headers = _get_headers(request)
 
-    admin_url = "admin/" if _is_admin(request) else ""
+    admin_url = (
+        "admin/" if _is_admin(request) or _get_group(request) in SUPPORT_GROUPS else ""
+    )
     lang = request.LANGUAGE_CODE.upper()
 
     response = requests.get(
