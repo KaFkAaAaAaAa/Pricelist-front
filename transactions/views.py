82b5367e155400db5fdcb8c1624cd581c173def0
Kasper Goździk <gozdzik.kasper@gmail.com>
2025-05-20 15:52:58 +0200

    minor fixes offer

--- transactions/views.py
+++ transactions/views.py
@@ -13,14 +13,31 @@ from django.utils.translation import gettext_lazy as _
 
 from items.views import _add_items_to_offer, _make_price_list
 from pdfgenerator.views import generate_pdf
-from Pricelist.settings import (ADMIN_GROUPS, API_BASE_URL, CLIENT_GROUPS,
-                                SKU_REGEX, SUPPORT_GROUPS, TRANSACTION_FINAL)
-from Pricelist.utils import (Page, _amount_to_display, _amount_to_float,
-                             _amount_to_store, _api_error_interpreter,
-                             _get_group, _get_headers, _get_page_param,
-                             _is_admin, _make_api_request, _price_to_display,
-                             _price_to_float, _price_to_store, require_auth,
-                             require_group)
+from Pricelist.settings import (
+    ADMIN_GROUPS,
+    API_BASE_URL,
+    CLIENT_GROUPS,
+    SKU_REGEX,
+    SUPPORT_GROUPS,
+    TRANSACTION_FINAL,
+)
+from Pricelist.utils import (
+    Page,
+    _amount_to_display,
+    _amount_to_float,
+    _amount_to_store,
+    _api_error_interpreter,
+    _get_group,
+    _get_headers,
+    _get_page_param,
+    _is_admin,
+    _make_api_request,
+    _price_to_display,
+    _price_to_float,
+    _price_to_store,
+    require_auth,
+    require_group,
+)
 from transactions.forms import STATUSES, ItemForm, PrognoseFrom, StatusForm
 
 logger = logging.getLogger(__name__)
@@ -158,8 +175,8 @@ def offer(request):
 
     if request.method == "POST":
         _add_items_to_session(request)
-    __import__('pdb').set_trace()
-    elif request.method == "POST" and not _is_admin(request):
+    __import__("pdb").set_trace()
+    if request.method == "POST" and not _is_admin(request):
         items, _ = _parse_transaction_edit_items(request)
         payload = {
             "description": request.POST["transaction_description"],
